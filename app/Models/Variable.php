f9591858d8835da42f0b3414027d52bcf471efb3
leenaa46 <hsherlock22@gmail.com>
2022-12-10 11:01:05 +0700

    Seed Activity

--- app/Models/Variable.php
+++ app/Models/Variable.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Variable extends BaseModel
 {
     use HasFactory;
+
+    public $image_logo_collection_name = 'LOGO';
+
+    protected $with = ['image_logo'];
+
+    public function image_logo()
+    {
+        return $this->morphOne(config('media-library.media_model'), 'model')
+            ->where('collection_name', $this->image_logo_collection_name);
+    }
 }
--- database/migrations/2022_12_09_133906_create_variables_table.php
+++ database/migrations/2022_12_09_133906_create_variables_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -15,7 +15,12 @@ return new class extends Migration
     {
         Schema::create('variables', function (Blueprint $table) {
             $table->id();
+            $table->string('type');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->foreignId('company_id')->nullable()->references('id')->on('companies');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
