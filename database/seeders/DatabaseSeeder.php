f91ffd01313128ef44e430ab544122fcf2a85e59
leenaa46 <hsherlock22@gmail.com>
2022-12-10 10:28:01 +0700

    Seed Example Company

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,11 +6,14 @@ namespace Database\Seeders;
 
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use App\Traits\ImageTrait;
 use App\Models\User;
 use App\Models\Company;
 
 class DatabaseSeeder extends Seeder
 {
+    use ImageTrait;
+
     /**
      * Seed the application's database.
      *
@@ -22,15 +25,23 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
         ]);
 
-        $company = Company::create([
-            'joinable_code' => Str::random(7)
-        ]);
+        $companies = 5;
+        foreach (\range(1, $companies) as $i) {
+            $company = Company::create([
+                'joinable_code' => "JOIN$i",
+                'name' => "Joinable $i",
+                "slogan" => "Innovate Business Solutions."
+            ]);
+            $this->addToModel("https://source.unsplash.com/random/400x400/?brand", $company, $company->image_profile_collection_name);
 
-        User::create([
-            'company_id' => $company->id,
-            'name' => 'admin',
-            'email' => 'admin@example.com',
-            'password' => \bcrypt('11111111')
-        ])->assignRole('admin');
+            User::create([
+                'company_id' => $company->id,
+                'name' => "joinable_$i",
+                'email' => "joinable_$i@example.com",
+                'password' => \bcrypt('11111111')
+            ])->assignRole('admin');
+
+            echo ("Seed Company $i/$companies \n");
+        }
     }
 }
